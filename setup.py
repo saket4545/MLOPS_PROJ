b2bdef17807df8e2de19029f24b0952bc672e8bc
saket4545 <saketmotekar1@gmail.com>
2025-01-13 19:29:56 +0530

    project stucture ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saket Motekar",
+    author_email="saketmotekar1@gmail.com",
+    packages=find_packages()
+)
